6354dad7c658288cabc7c1bb28999f329a469c41
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-17 22:03:56 +0800

    modify user entity
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/main/java/com/thoughtworks/User.java
+++ src/main/java/com/thoughtworks/User.java
@@ -1,4 +1,19 @@
 package com.thoughtworks;
 
 public class User {
+    private long id;
+    private String name;
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
