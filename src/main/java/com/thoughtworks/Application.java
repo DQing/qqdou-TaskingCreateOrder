c18a2c236cfc123f767c6f28f44ee899cf9b769b
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-17 21:45:05 +0800

    finish get product price test and implement
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/main/java/com/thoughtworks/Application.java
+++ src/main/java/com/thoughtworks/Application.java
@@ -1,4 +1,22 @@
 package com.thoughtworks;
 
-public class Application {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Application {
+    List<HashMap> getProductPrice(List<Product> products, HashMap<Long, Integer> input) {
+        List<HashMap> result = new ArrayList<>();
+        products.forEach(product -> {
+            long productId = product.getId();
+            if (input.containsKey(productId)) {
+                HashMap<String, Object> map = new HashMap<>();
+                int productPrice = product.getPrice() * input.get(productId);
+                map.put("productPrice", productPrice);
+                map.put("product", product);
+                result.add(map);
+            }
+        });
+        return result;
+    }
 }
--- src/test/java/com/thoughtworks/ApplicationTest.java
+++ src/test/java/com/thoughtworks/ApplicationTest.java
@@ -1,4 +1,37 @@
 package com.thoughtworks;
 
-public class ApplicationTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ApplicationTest {
+    private Application application = new Application();
+    private List<Product> products;
+    @BeforeEach
+    void init() {
+        products = Arrays.asList(new Product(1, "pencil", 2),
+                new Product(2, "phone", 1600),
+                new Product(3, "cake", 10),
+                new Product(4, "water", 3),
+                new Product(5, "table", 100));
+    }
+    @Test
+    void should_get_products_totalPrice() {
+        HashMap<Long, Integer> input = new HashMap<>();
+        input.put(1L, 2);
+        input.put(2L, 1);
+        List<HashMap> productPrice = application.getProductPrice(products, input);
+
+        assertEquals(2, productPrice.size());
+        assertEquals(productPrice.get(0).get("productPrice"), 4);
+        assertEquals(productPrice.get(0).get("product"), products.get(0));
+        assertEquals(productPrice.get(1).get("productPrice"), 1600);
+        assertEquals(productPrice.get(1).get("product"), products.get(1));
+    }
 }
